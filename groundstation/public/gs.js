b1d905c3048264c081b3758284cc65d2d725a3d6
Oscar Fawkes <oscar@outoftime.co>
2019-05-17 17:06:33 +1000

    gs update

--- groundstation/public/gs.js
+++ groundstation/public/gs.js
@@ -1,1 +1,9 @@
-
+let url = window.location.href.split(':')[1].split('/').pop();
+console.log(url)
+var socket = new WebSocket("ws://"+url+":9876/ws");
+socket.onmessage = function (event) {
+  console.log(event.data);
+}
+// setInterval(function (){
+//     console.log(socket.readyState);
+// },2000);

220c2f3bb1790b4092931b899c030ffc14a52dbd
Oscar Fawkes <oscar@outoftime.co>
2019-05-19 00:07:18 +1000

    update all_test for full wod downlink

--- test code/ALL_TEST/ALL_TEST.ino
+++ test code/ALL_TEST/ALL_TEST.ino
@@ -4,6 +4,8 @@
 #include <Adafruit_INA219.h>
 #include <TinyGPS.h>
 
+#define WOD_TRANSMIT_BUF_LEN (81)
+
 
 //GLOBALS
 
@@ -25,12 +27,15 @@ Adafruit_INA219 ina219_bat(0x40);
 Adafruit_INA219 ina219_3v3(0x41);
 Adafruit_INA219 ina219_5v(0x44);
 
+//TRANSMIT BUFFER
+char transmit[WOD_TRANSMIT_BUF_LEN];
+
 
 //SETUP
 void setup() {
   //GENERIC
   Serial.begin(115200);
-  while(!Serial);
+  //while(!Serial); //will hang if teensy not connected to pc
 
   pinMode(13, OUTPUT);
   led = 0;
@@ -94,6 +99,8 @@ void loop() {
           
           if (gps.encode(c)) {
             Serial.println("Updated");
+            gps.get_position(&lat, &lon, &fix_age);
+            gps.get_datetime(&date, &time, &fix_age);
             sprintf(s, "Lat: %ld Lon: %ld Time: %ld", lat, lon, time);
             Serial.println(s);
             Serial1.println(s);
@@ -106,7 +113,7 @@ void loop() {
 
           if (fix_age == TinyGPS::GPS_INVALID_AGE) {
             Serial.println("No fix detected");
-            Serial1.println("NFD");
+            //Serial1.println("NFD");
             Serial.print("Time: ");
             Serial.println(time);
           }
@@ -121,7 +128,7 @@ void loop() {
 
   //EPS
 
-  float batVoltage = ina219_bat.getBusVoltage_V();
+  
   
   Serial.print("Battery Voltage:   "); Serial.print(ina219_bat.getBusVoltage_V()); Serial.println(" V");
   Serial.print("Battery Current:       "); Serial.print(ina219_bat.getCurrent_mA()); Serial.println(" mA");
@@ -130,11 +137,61 @@ void loop() {
   Serial.print("3V3 Voltage:   "); Serial.print(ina219_3v3.getBusVoltage_V()); Serial.println(" V");
   Serial.print("3V3 Current:       "); Serial.print(ina219_3v3.getCurrent_mA()); Serial.println(" mA");
 
-  Serial1.print("~~~");
-  for (char i = 0; i < 4; i++) {
-    Serial1.print(((char*)&batVoltage)[i]);
+  // WOD send
+  
+  long long time_send = 0;
+  if (fix_age == TinyGPS::GPS_INVALID_AGE) {
+    *((int*)&transmit[0]) = millis();
+    *((int*)&transmit[2]) = millis();
+    *((int*)&transmit[4]) = micros();
+    *((int*)&transmit[6]) = micros();
+  } else {
+    time_send = time;
   }
   
+  transmit[8] = 0xFF;
+  transmit[9] = 0x00;
+
+  *((float*)&transmit[9]) = ina219_bat.getBusVoltage_V();
+  *((float*)&transmit[13]) = ina219_bat.getCurrent_mA();
+  *((float*)&transmit[17]) = 12.34;
+
+  *((float*)&transmit[21]) = ina219_5v.getBusVoltage_V();
+  *((float*)&transmit[25]) = ina219_5v.getCurrent_mA();
+  *((float*)&transmit[29]) = ina219_3v3.getBusVoltage_V();
+  *((float*)&transmit[33]) = ina219_3v3.getCurrent_mA();
+  *((float*)&transmit[37]) = 23.45;
+
+  *((float*)&transmit[41]) = 34.56;
   
-  delay(100);
+  unsigned long fixAge;
+
+  float latitude, longitude;
+  gps.f_get_position(&latitude, &longitude, &fixAge);
+
+  if (fixAge == TinyGPS::GPS_INVALID_AGE) {
+    *((float*)&transmit[45]) = 360.0;
+    *((float*)&transmit[49]) = 360.0;
+    *((float*)&transmit[53]) = -1.0;
+  } else {
+    *((float*)&transmit[45]) = latitude;
+    *((float*)&transmit[49]) = longitude;
+    *((float*)&transmit[53]) = gps.f_altitude();
+  }
+
+  *((float*)&transmit[57]) = 45.67;
+  *((float*)&transmit[61]) = 56.78;
+  *((float*)&transmit[65]) = 67.89;
+  *((float*)&transmit[69]) = IMU.getGyroZ_rads();
+  *((float*)&transmit[73]) = IMU.getGyroY_rads();
+  *((float*)&transmit[77]) = IMU.getGyroX_rads();
+
+  Serial1.write(0x7E); //flag
+  for (char i = 0; i < WOD_TRANSMIT_BUF_LEN; i++) {
+    Serial1.write(transmit[i]);
+    Serial.write(transmit[i]);
+  }
+  Serial1.write(0x7E); //flag
+  
+  //delay(1000);
 }
